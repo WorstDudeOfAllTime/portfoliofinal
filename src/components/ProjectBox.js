07652f9ffac0bf6754bab86f4a1a058bfa1af967
Worstdudeofalltime <Krisbertolino@gmail.com>
2022-10-07 21:23:56 -0400

    index.html corrections

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -5,7 +5,7 @@ const ProjectBox = ({ project}) => {
     <div className="projectBoxContainer">
       <div className="boxHalf left">
         <a href={project.link} target="blank">
-          <img className="projectImage" src={project.imgSrc} alt="project image"/>
+          <img className="projectImage" src={project.imgSrc} alt="project"/>
         </a>
       </div>
       <div className="boxHalf right">
